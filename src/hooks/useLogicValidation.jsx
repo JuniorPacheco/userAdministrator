589d8aa9dab7916e81ebc31d351239b3c279e8f6
Junior Pacheco <junior.pacheco.356@gmail.com>
2022-06-19 17:12:20 -0500

    Pryecto finalizado Petro Presidente

--- src/hooks/useLogicValidation.jsx
+++ src/hooks/useLogicValidation.jsx
@@ -6,11 +6,11 @@ const schema = yup.object({
     first_name: yup.string().max(25, 'Very large first name').required('First name is required'),
     last_name: yup.string().max(25, 'Very large last name').required('Last name is required'),
     email: yup.string().email('Not a correct format').required('Email is required'),
-    password: yup.string().matches(/^[\S]{8,}$/, 'Should contain at least 8 characters')
+    password: yup.string().min(8, 'Should contain at least 8 characters')
+                          .max(25, 'Must contain less than 25 characters')
                           .matches(/^(?=.*\d)/, 'Should contain at least one digit')
                           .matches(/^(?=.*[a-z])/, 'Should contain at least one lower case')
-                          .matches(/^(?=.*[A-Z])/, 'Should contain at least one upper case')
-                          .max(40, 'Very long password'), 
+                          .matches(/^(?=.*[A-Z])/, 'Should contain at least one upper case'), 
     birthday: yup.string().required('Birthday is required')
   }).required()
 
